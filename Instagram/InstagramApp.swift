ce5df241a854fe14b0df6523c4ad74e6614a09dd
yonashstan <yonik33333@gmail.com>
2023-10-22 23:02:46 +0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Yonash Stan on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
